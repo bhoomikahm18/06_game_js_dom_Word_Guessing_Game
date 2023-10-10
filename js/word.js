0405c7bca6f876bb33f7ff1f4045c42f0a643958
Bhoomika <bhoomikahm18@gmail.com>
2023-10-10 10:27:29 +0530

    Adding Words with hints to the js file

---
+++ js/word.js
@@ -0,0 +1,86 @@
+const wordList = [
+    {
+        word: "python",
+        hint: "programming language"
+    },
+    {
+        word: "guitar",
+        hint: "a musical instrument"
+    },
+    {
+        word: "aim",
+        hint: "a purpose or intention"
+    },
+    {
+        word: "coding",
+        hint: "related to programming"
+    },
+    {
+        word: "venus",
+        hint: "planet of our solar system"
+    },
+    {
+        word: "gold",
+        hint: "a yellow precious metal"
+    },
+    {
+        word: "map",
+        hint: "diagram representation of area"
+    },
+    {
+        word: "matrix",
+        hint: "science fiction movie"
+    },
+    {
+        word: "bugs",
+        hint: "related to program"
+    },
+    {
+        word: "avatar",
+        hint: "apic science fiction film"
+    },
+    {
+        word: "metal",
+        hint: "relating to the mind"
+    },
+    {
+        word: "java",
+        hint: "programming language"
+    },
+    {
+        word: "hockey",
+        hint: "a famous outdoor game"
+    },
+    {
+        word: "act",
+        hint: "the doing of a thing"
+    },
+    {
+        word: "github",
+        hint: "code histing platform"
+    },
+    {
+        word: "chess",
+        hint: "game played by two players"
+    },
+    {
+        word: "idea",
+        hint: "a thought or suggestion"
+    },
+    {
+        word: "server",
+        hint: "related to computer or system"
+    },
+    {
+        word: "svg",
+        hint: "a vector image format"
+    },
+    {
+        word: "jpeg",
+        hint: "a small image file format"
+    },
+    {
+        word: "search",
+        hint: "act to find something"
+    }
+]
