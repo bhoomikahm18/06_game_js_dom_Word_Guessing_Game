a8973655768c24579a44ae57eeabd316b509229c
Bhoomika <bhoomikahm18@gmail.com>
2023-10-10 11:04:44 +0530

    Showing matched letter in the input value

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 const inputs = document.querySelector(".inputs"),
 resetBtn = document.querySelector(".reset-btn"),
-hint = document.querySelector(".hint span");
+hint = document.querySelector(".hint span"),
+typingInput = document.querySelector(".typing-input");
+
+let word;
 
 function randomWord() {
     //getting random object from wordlist
     let ranObj = wordList[Math.floor(Math.random() * wordList.length)];
-    let word = ranObj.word; //getting word of random object
-    console.log(ranObj);
+    word = ranObj.word; //getting word of random object
+    console.log(word);
 
     hint.innerText = ranObj.hint;
 
@@ -19,4 +22,23 @@ function randomWord() {
 }
 randomWord();
 
+function initGame(e) {
+    let key = e.target.value;
+    if(key.match(/^[A-Za-z]+$/)){
+        console.log(key);
+        if(word.includes(key)){ //if user letter found in the word
+           for (let i = 0; i < word.length; i++) {
+            //Showing matched letter in the input  value
+            if(word[i] === key){
+                inputs.querySelectorAll("input")[i].value = key;
+            }
+           }
+        }else{  
+            console.log("Letter not found");
+        }
+    }
+}
+
 resetBtn.addEventListener("click", randomWord);
+typingInput.addEventListener("input", initGame);
+document.addEventListener("keydown", ()=>typingInput.focus());
