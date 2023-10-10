8306648b2a64b6f4c6fab632d11b1b16a3bdc6d1
Bhoomika <bhoomikahm18@gmail.com>
2023-10-10 11:33:59 +0530

    Alert window if user couldn't found all letters

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,22 @@
 const inputs = document.querySelector(".inputs"),
     resetBtn = document.querySelector(".reset-btn"),
     hint = document.querySelector(".hint span"),
+    guessLeft = document.querySelector(".guess-left span"),
     wrongletter = document.querySelector(".wrong-letter span"),
     typingInput = document.querySelector(".typing-input");
 
-let word, currects = [], incurrects = [];
+let word, maxGuesses, currects = [], incurrects = [];
 
 function randomWord() {
     //getting random object from wordlist
     let ranObj = wordList[Math.floor(Math.random() * wordList.length)];
     word = ranObj.word; //getting word of random object
+    maxGuesses = 8; currects = []; incurrects = [];
     console.log(word);
 
     hint.innerText = ranObj.hint;
+    guessLeft.innerText = maxGuesses;
+    wrongletter.innerText = incurrects;
 
     let html = "";
     for (let i = 0; i < word.length; i++) {
@@ -36,11 +40,21 @@ function initGame(e) {
                 }
             }
         } else {
+            maxGuesses--; //decrement maxGuesses by 1
             incurrects.push(` ${key}`);
         }
+        guessLeft.innerText = maxGuesses;
+        wrongletter.innerText = incurrects;
     }
-    wrongletter.innerText = incurrects;
     typingInput.value = "";
+
+    if(maxGuesses < 1){ //if user couldn't found all letters
+        alert("Game Over! You don't have remaining guesses")
+        for (let i = 0; i < word.length; i++) {
+            //Showing all letter in the input
+            inputs.querySelectorAll("input")[i].value = word[i];
+        }
+    }
 }
 
 resetBtn.addEventListener("click", randomWord);
